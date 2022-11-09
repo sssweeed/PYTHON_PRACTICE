04a07155d0058b94abc5cdb45dc52a699e35d565
sssweeed <117772484+sssweeed@users.noreply.github.com>
2022-11-09 12:11:24 +0300

    Update main.py

--- release-1/main.py
+++ release-1/main.py
@@ -1,2 +1,32 @@
+a = input()
 def hiFunction(name):
-    print()
+    return("Hi, " + name)
+print(hiFunction(a))
+
+
+a = int(input())
+b = int(input())
+def sum(x, y):
+    return(x + y)
+print(sum(a, b))
+
+
+x = int(input())
+def isEven(x):
+    if x % 2 == 0:
+        return "true"
+    else:
+        return "false"
+print(isEven(x))
+
+
+x = int(input())
+def apples(x):
+    return "i have", x, "apples"
+print(apples(x))
+
+
+x = int(input())
+def getPower(x):
+    return x ** 2
+print(getPower(x))
